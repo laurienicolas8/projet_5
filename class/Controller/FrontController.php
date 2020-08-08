9fede5403e5602af9893b9576d5ea1f51067fa1c
Laurie Nicolas <laurienicolas8@gmail.com>
2020-08-08 16:41:51 +0200

    empty error

--- class/Controller/FrontController.php
+++ class/Controller/FrontController.php
@@ -12,26 +12,28 @@ use App\Entity\Quiz;
 require('vendor/autoload.php');
 
 class FrontController extends Controller {
-    
+        
+    /**
+     * home
+     * fetch all categories and creating object if the category is not empty
+     * fetch the home quiz
+     * delete duplications in the array categories for the quiz cards in home
+     * render the page home
+     * @return void
+     */
     public function home() {
         $categories = $this->categoryDAO->getAllCategories();
         foreach ($categories as $category) {
-            // Si une catégorie ne contient aucun quiz, alors elle n'apparait pas
-            $quiz = $this->quizByCategoryObject($category['idCategory']);
-            if ($quiz !== null) { 
-                $allCategories[] = new Category($category);
-            }
+            $allCategories[] = new Category($category);
         }
         $allQuiz = $this->quizDAO->getHomeQuiz();
         foreach ($allQuiz as $quiz) {
             $questions = $this->questionsObject($quiz['idQuiz']);
-            if ($questions !== null) {
-                // Récupérer le nom de la catégorie de chaque quiz
-                $quizCategory[] = $quiz['idCategory'];
-                $homeQuiz[] = new Quiz($quiz);
-            }
+            // I fetch the category of the quiz
+            $quizCategory[] = $quiz['idCategory'];
+            $homeQuiz[] = new Quiz($quiz);
         }
-        // Supprimer les doublons si 2 quiz ont la même catégorie
+        // Delete duplications to avoid duplications in quiz cards
         $quizUnique = array_unique($quizCategory);
         foreach ($quizUnique as $quiz) {
             $singleCategory = $this->categoryDAO->getSingleCategory($quiz);
@@ -46,19 +48,24 @@ class FrontController extends Controller {
             'oneCategory' => $oneCategory,
         ]);
     }
-
+    
+    /**
+     * allQuiz
+     * fetch all quiz of the current page
+     * check if the quiz contents questions
+     * delete the duplication in the array categories for the quiz cards
+     * render the page all_quiz
+     * @param  int $currentPage
+     * @return void
+     */
     public function allQuiz($currentPage) {
         try {
             $quiz = $this->quizDAO->getAllQuiz($currentPage);
             foreach ($quiz as $oneQuiz) {
-                // Si un quiz ne contient aucune question, alors il n'apparait pas
-                $questions = $this->questionsObject($oneQuiz['idQuiz']);
-                if ($questions !== null) {
-                   $quizCategory[] = $oneQuiz['idCategory'];
-                    $allQuiz[] = new Quiz($oneQuiz); 
-                }
+                $quizCategory[] = $oneQuiz['idCategory'];
+                $allQuiz[] = new Quiz($oneQuiz);
             }
-            // Supprimer les doublons pour éviter les doublons de catégorie dans la view
+            // Delete duplications to avoid duplications in the quiz cards in view
             $quizUnique = array_unique($quizCategory);
             foreach ($quizUnique as $quiz) {
                 $singleCategory = $this->categoryDAO->getSingleCategory($quiz);
@@ -76,19 +83,26 @@ class FrontController extends Controller {
             echo 'Erreur controller : La numéro de la page est inconnu';
         }
     }
-
+    
+    /**
+     * detailSingleCategory
+     * fetch the category concerned and all its quiz
+     * create objects
+     * render the page single_category
+     * @param  int $idCategory
+     * @return void
+     */
     public function detailSingleCategory($idCategory) {
         try {
             $oneCategory = $this->singleCategoryObject($idCategory);
             $quiz = $this->quizDAO->getQuizByCategory($idCategory);
-            $allQuiz = [];
-            foreach ($quiz as $oneQuiz) {
-                $questions = $this->questionsObject($oneQuiz['idQuiz']);
-                if ($questions !== null) {
-                    $allQuiz[] = new Quiz($oneQuiz); 
-                } 
+            if ($quiz !== []) {
+                foreach ($quiz as $oneQuiz) {
+                    $allQuiz[] = new Quiz($oneQuiz);  
+                }
+            } else {
+                $allQuiz = null;
             }
-            
             echo $this->twig->render('single_category.twig', [
                 'oneCategory' => $oneCategory,
                 'allQuiz' => $allQuiz,
@@ -97,7 +111,16 @@ class FrontController extends Controller {
             echo 'Erreur controller : Aucune catégorie identifiée';
         }
     }
-
+    
+    /**
+     * startQuiz
+     * fetch the quiz concerned and count its questions
+     * create a session variable of all the questions of the quiz
+     * fetch the first index / the first question
+     * render the page start_quiz
+     * @param  int $idQuiz
+     * @return void
+     */
     public function startQuiz($idQuiz) {
         try {
             $oneQuiz = $this->singleQuizObject($idQuiz);
@@ -107,9 +130,13 @@ class FrontController extends Controller {
             }
             $allQuestions = $this->questionsObject($idQuiz);
             $_SESSION['quiz_questions'] = $allQuestions;
-            // Je récupère le premier index pour accéder à la première question du quiz
+            // I fetch the first index to access the first question of the quiz
             $questions = $_SESSION['quiz_questions'];
-            $firstQuestionIndex = array_key_first($questions);
+            if ($questions !== null) {
+                $firstQuestionIndex = array_key_first($questions);
+            } else {
+                $firstQuestionIndex = null;
+            }
             echo $this->twig->render('start_quiz.twig', [
                 'oneQuiz' => $oneQuiz,
                 'questions' => $questions,
@@ -121,21 +148,35 @@ class FrontController extends Controller {
             echo 'Erreur controller : Aucun quiz identifié';
         }
     }
-
+    
+    /**
+     * question
+     * fetch the quiz concerned
+     * check that the questions has been fetched in session variable quiz_questions
+     * fetch the id of the current question and its answer
+     * render the page questions
+     * @param  int $indexQuestion
+     * @param  int $idQuiz
+     * @return void
+     */
     public function question($indexQuestion, $idQuiz) {
         try {
             $oneQuiz = $this->singleQuizObject($idQuiz);
-            // Je vérifie si les questions ont bien été récupérées dans start_quiz
+            // I check that the questions has been fetched in start_quiz
             if (isset($_SESSION['quiz_questions'])) {
                 $questions = $_SESSION['quiz_questions'];
-                // Je récupère l'id de la question actuelle pour la récupérer en base
+                // I fetch the id of the current question to fetch it in database
                 $idCurrentQuestion = $questions[$indexQuestion]->idQuestion();
-                // Je récupère la question actuelle et ses réponses pour les afficher
+                // I fetch the current question and its answers to display
                 $singleQuestion = $this->singleQuestionObject($idCurrentQuestion);
                 foreach ($singleQuestion as $question) {
                     $answers = $this->answerDAO->getQuestionAnswers($question->idQuestion());
-                    foreach ($answers as $answer) {
-                        $allAnswers[] = new Answer($answer);
+                    if ($answers !== []) {
+                        foreach ($answers as $answer) {
+                            $allAnswers[] = new Answer($answer);
+                        }
+                    } else {
+                        $allAnswers = null;
                     }
                 }
             } else {
@@ -152,17 +193,27 @@ class FrontController extends Controller {
             echo 'Erreur controller : Aucun quiz identifié';
         }
     }
-
+    
+    /**
+     * checkAnswer
+     * fetch the right answer
+     * create an object
+     * compare id right answer and id player answer
+     * create a session variable right answer or bad answer
+     * @param  int $idAnswer
+     * @param  int $idQuestion
+     * @return void
+     */
     public function checkAnswer($idAnswer, $idQuestion) {
         try {
-            // Je récupère la bonne réponse à la question en cours
+            // I fetch the right answer of the current question
             $rightAnswerQuestion = $this->answerDAO->getRightAnswer($idQuestion);
-            // J'en fais un objet pour récupérer son id
+            // I make an object to fetch its id
             foreach ($rightAnswerQuestion as $answer) {
                 $rightAnswer[] = new Answer($answer);
             }
             $idRightAnswer = $rightAnswer[0]->idAnswer();
-            // Je compare son id avec celui de la réponse sélectionnée par le visiteur
+            // I compare its id with the id of the player's answer
             if ($idRightAnswer == $idAnswer) {
                 $this->resultDAO->addResult(1);
                 $_SESSION['right_answer'] = 'Bonne réponse !';
@@ -174,24 +225,37 @@ class FrontController extends Controller {
             echo 'Erreur : Aucune réponse n\'a été sélectionnée';
         }
     }
-
+    
+    /**
+     * answer
+     * fetch the quiz concerned
+     * check if the questions of the quiz has been fetched in session variable
+     * fetch the last index in the array to know where the quiz takes end
+     * define the next question index
+     * fetch the question and its answers and creating objects with controller method
+     * render the page answer
+     * @param  string $answerPlayer
+     * @param  int $indexQuestion
+     * @param  int $idQuiz
+     * @return void
+     */
     public function answer($answerPlayer, $indexQuestion, $idQuiz) {
         try {
             $oneQuiz = $this->singleQuizObject($idQuiz);
-            // Je vérifie si les questions ont bien été récupérées dans start_quiz
+            // I check that the questions has been fetched in start_quiz
             if (isset($_SESSION['quiz_questions'])) {
-                // Je récupère la dernière clé du tableau pour adapter le bouton quand toutes les questions auront été posées
+                // I fetch the last in the array to adapt the button at the end of the quiz
                 $questions = $_SESSION['quiz_questions'];
                 $lastQuestionIndex = array_key_last($questions);
-                // Je récupère l'id de la question actuelle pour la récupérer en base
+                // I fetch the current question id to fetch it in database
                 $idCurrentQuestion = $questions[$indexQuestion]->idQuestion();
-                // Je définis l'index de la question suivante
+                // I define the next question index
                 if ($indexQuestion + 1 <= $lastQuestionIndex) {
                     $nextQuestionIndex = $indexQuestion + 1;
                 } else {
                     $nextQuestionIndex = null;
                 }
-                // Je récupère la question actuelle et ses réponses pour les afficher
+                // I fetch the current question and its answers to display
                 $singleQuestion = $this->singleQuestionObject($idCurrentQuestion);
                 foreach ($singleQuestion as $question) {
                     $answers = $this->answerDAO->getQuestionAnswers($question->idQuestion());
@@ -217,7 +281,17 @@ class FrontController extends Controller {
             echo 'Erreur controller : Aucun quiz identifié';
         }
     }
-
+    
+    /**
+     * result
+     * fetch the quiz concerned and the count of its questions
+     * fetch all points won on the quiz
+     * calculating the sum of all points
+     * render the page result
+     * then delete all results and delete the session variable of the quiz questions
+     * @param  int $idQuiz
+     * @return void
+     */
     public function result($idQuiz) {
         try {
             $oneQuiz = $this->singleQuizObject($idQuiz);
